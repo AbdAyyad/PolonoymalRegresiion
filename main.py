c040b21e6b8c2ae571f658e8ee8c16bb311eae7f
AbdAyyad <abdayyad2012@gmail.com>
2019-04-14 23:04:11 +0300

    visualizing the data and set degree to 3

--- main.py
+++ main.py
@@ -24,4 +24,17 @@ x_poly = poly_reg.fit_transform(X)
 lin_reg_2 = LinearRegression()
 lin_reg_2.fit(x_poly, y)
 
-# visulize data
+# visualising linear data
+plt.scatter(X, y, color='red')
+plt.plot(X, lin_reg.predict(X), color='blue')
+plt.title('truth of bluff (Linear Regression)')
+plt.xlabel('position level')
+plt.ylabel('salary')
+plt.show()
+
+plt.scatter(X, y, color='red')
+plt.plot(X, lin_reg_2.predict(x_poly), color='blue')
+plt.title('truth of bluff (Polynomial Regression)')
+plt.xlabel('position level')
+plt.ylabel('salary')
+plt.show()
